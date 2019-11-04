5214c55f311c8dad722ece5167fcced31c14f8e3
Ubuntu <ubuntu@ip-172-31-88-101.ec2.internal>
2019-11-04 01:40:48 +0000

    “Changes”

---
+++ full_name.rb
@@ -0,0 +1,30 @@
+puts "What is your first name?"
+x = []
+first_name = gets.chomp
+x << first_name
+puts "What is your middle name?"
+middle_name = gets.chomp
+x << middle_name
+puts "What is your last name"
+last_name = gets.chomp
+x << last_name
+
+puts "Hi #{x.inspect}"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
